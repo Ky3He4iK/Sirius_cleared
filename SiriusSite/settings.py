75fd1033bd887a4c63e5d14627c3b0029ec0c6f6
ky3he4ik <misha10311@yandex.ru>
2018-12-19 22:16:49 +0500

    added automatic ip detection to ALLOWED_HOSTS

--- SiriusSite/settings.py
+++ SiriusSite/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from socket import gethostname, gethostbyname
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -26,8 +26,9 @@ SECRET_KEY = os.path.join(BASE_DIR, "SiriusSite/secret/key")
 DEBUG = True
 
 ALLOWED_HOSTS = [
+    gethostname(),
+    gethostbyname(gethostname()),
     "localhost",
-    "10.10.201.154",
 ]
 
 # Application definition
@@ -73,7 +74,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'SiriusSite.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
@@ -83,7 +83,6 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
@@ -116,7 +115,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
